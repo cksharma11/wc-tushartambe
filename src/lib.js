28fd4782302b0fcf5e1b14c56f0ae35a87858f1f
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2019-01-03 13:12:40 +0530

    Refactor code with readFile. Introduced readFileContents() sequenceGenerator().

--- src/lib.js
+++ src/lib.js
@@ -1,74 +1,104 @@
 const { parseInputs } = require("./parser");
-const {
-	readFile,
-	countBytes,
-	countLines,
-	countWords
-} = require("./util");
+const { countBytes, countLines, countWords } = require("./util");
 
-const isOptionExists = function (options, option) {
-	return options.includes(option);
-}
+const isOptionExists = function(options, option) {
+  return options.includes(option);
+};
 
-const resultGenerator = function (fs, options, file) {
+const joinWithTab = function(list) {
+  return list.join("\t");
+};
 
-	const fileContents = readFile(file, fs);
-	let isOptionPresent = isOptionExists.bind(null, options);
-	let wcOutput = [];
+const joinWithNewLine = function(list) {
+  return list.join("\n");
+};
 
-	if (isOptionPresent('l')) {
-		wcOutput.push(countLines(fileContents));
-	}
+const calculateSum = function(firstList, secondList) {
+  let sum = [];
+  for (let counter = 0; counter < firstList.length - 1; counter++) {
+    sum[counter] = firstList[counter] + secondList[counter];
+  }
+  sum.push("total");
+  return sum;
+};
 
-	if (isOptionPresent('w')) {
-		wcOutput.push(countWords(fileContents));
-	}
+const getCounts = function(options, fileName, fileContents) {
+  let isOptionPresent = isOptionExists.bind(null, options);
+  let wcOutput = [];
 
-	if (isOptionPresent('c')) {
-		wcOutput.push(countBytes(fileContents));
-	}
+  if (isOptionPresent("l")) {
+    wcOutput.push(countLines(fileContents));
+  }
+  if (isOptionPresent("w")) {
+    wcOutput.push(countWords(fileContents));
+  }
 
-	wcOutput.push(file);
-	return wcOutput;
-}
+  if (isOptionPresent("c")) {
+    wcOutput.push(countBytes(fileContents));
+  }
+  wcOutput.push(fileName);
 
-const joinWithTab = function (list) {
-	return list.join("\t");
-}
+  return wcOutput;
+};
+const sequenceGenerator = function(originalSequence, list) {
+  let fileIndex = list[0].length - 1;
+  let sequence = originalSequence.map(function(element) {
+    for (let counter = 0; counter < originalSequence.length; counter++) {
+      if (element == list[counter][fileIndex]) {
+        return list[counter];
+      }
+    }
+  });
+  return sequence;
+};
 
-const joinWithNewLine = function (list) {
-	return list.join("\n");
-}
+const reader = function(
+  options,
+  files,
+  totalCounts,
+  counter,
+  fs,
+  err,
+  fileContents
+) {
+  totalCounts.push(getCounts(options, files[counter], fileContents));
+  if (files.length == 1) {
+    let wcOutput = joinWithNewLine(totalCounts.map(joinWithTab));
+    console.log(wcOutput);
+    fs.writeFileSync("./output", wcOutput, "utf8");
+  }
 
-const calculateSum = function (firstList, secondList) {
-	let sum = [];
-	for (let counter = 0; counter < firstList.length - 1; counter++) {
-		sum[counter] = firstList[counter] + secondList[counter];
-	}
-	sum.push("total");
-	return sum;
-}
+  if (files.length > 1 && totalCounts.length == files.length) {
+    let sequencedTotalCounts = sequenceGenerator(files, totalCounts);
+    let total = sequencedTotalCounts.reduce(calculateSum);
+    sequencedTotalCounts.push(total);
+    let wcOutput = joinWithNewLine(sequencedTotalCounts.map(joinWithTab));
+    console.log(wcOutput);
 
-const wc = function (args, fs) {
-	let organizedInputs = parseInputs(args);
+    fs.writeFileSync("./output", wcOutput, "utf8");
+  }
+};
 
-	const { files, options, errorMsg } = organizedInputs;
+const readFileContents = function(options, files, fs) {
+  let totalCounts = [];
+  let fileReader;
+  for (let counter = 0; counter < files.length; counter++) {
+    fileReader = reader.bind(null, options, files, totalCounts, counter, fs);
+    fs.readFile(files[counter], "utf8", fileReader);
+  }
+};
 
-	if (errorMsg) {
-		return errorMsg;
-	}
+const wc = function(args, fs) {
+  let organizedInputs = parseInputs(args);
 
-	let getResult = resultGenerator.bind(null, fs, options);
-	let wcOutput = files.map(getResult);
+  const { files, options, errorMsg } = organizedInputs;
 
-	if (files.length > 1) {
-		let total = wcOutput.reduce(calculateSum);
-		wcOutput.push(total);
-	}
-
-	return joinWithNewLine(wcOutput.map(joinWithTab));
-}
+  if (errorMsg) {
+    console.log(errorMsg);
+  }
+  readFileContents(options, files, fs);
+};
 
 module.exports = {
-	wc
+  wc
 };
--- wc.js
+++ wc.js
@@ -1,9 +1,9 @@
-const { wc } = require('./src/lib');
-const fs = require('fs');
+const { wc } = require("./src/lib");
+const fs = require("fs");
 
 const main = function() {
   const args = process.argv.slice(2);
 
-  console.log(wc(args,fs));
-}
+  wc(args, fs);
+};
 main();
